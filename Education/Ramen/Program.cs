a9ea7fa85cab01cb5edaefd42797b644159105c3
harima-molgroup <takahisa.harima@molgroup.com>
2019-12-05 17:31:49 +0900

    課題を洗練

--- Education/Ramen/Program.cs
+++ Education/Ramen/Program.cs
@@ -8,131 +8,98 @@ namespace Ramen
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
-            string ramenName = (args != null || args.Length == 0) ? "shoyu" :
-                                (string.IsNullOrWhiteSpace(args[0])) ? "shoyu" :
-                                args[0];
+            var me = new Chef();
+            var ramen = me.Cook();
 
-            var oyaji = Order(ramenName);
-
-            var ramen = oyaji.Serve();
-            
             Console.ReadKey();
-
-            // ------------------------------------
-
-            oyaji = Order("ヤサイマシニンニクカラメスコシ");
-        }
-
-        private static Master Order(string ramenName)
-        {
-            var master = new Master();
-            master.OrderedMenu = ramenName;
-
-            if (master.Menu.ContainsKey(ramenName))
-            {
-                Console.WriteLine($"あいよ、{master.Menu[ramenName]}ラーメン一丁!");
-            }
-            else
-            {
-                throw new Exception($"うちには{ramenName}なんてラーメンはないよ!");
-            }
-
-            return master;
         }
     }
 
-    public class Master
+    public class Chef
     {
-        public Dictionary<string, string> Menu = new Dictionary<string, string> {
-            {"shoyu", "醤油" },{"miso", "みそ" }
-        };
-
-        public string OrderedMenu;
-
-        public Ramen Serve()
+        public ShoyuRamen Cook()
         {
-            var noodle = new Noodle("細麺", 150);
-            var soup = new Soup("醤油スープ", 200);
+            Console.WriteLine($"さあ、醤油ラーメンを作ろう!");
+
+            var noodle = new Noodle("細麺", 300);
+            var soup = new Soup("醤油スープ", 100);
 
             var toppings = new List<Topping>();
-            toppings.Add(new Topping("チャーシュー", 150));
-            toppings.Add(new Topping("メンマ", 100));
-            toppings.Add(new Topping("わかめ", 30));
-            toppings.Add(new Topping("のり", 50));
+            toppings.Add(new Topping("チャーシュー", 75));
+            toppings.Add(new Topping("メンマ", 50));
+            toppings.Add(new Topping("たまご", 100));
+            toppings.Add(new Topping("わかめ", 10));
+            toppings.Add(new Topping("のり", 10));
 
-            var ramen = new Ramen(noodle, soup, toppings);
-            ramen.Name = Menu[OrderedMenu];
+            var ramen = new ShoyuRamen(noodle, soup, toppings);
 
-            Console.WriteLine($"大変お待たせいたしました。{ramen.Name}ラーメンをお持ちしました。");
-            Console.WriteLine($"料金は{ramen.GetPrice()}円になります。");
+            Console.WriteLine($"醤油ラーメンができた。{ramen.GetCalory()}キロカロリー也。");
             return ramen;
         }
     }
-
-    public class Ramen
+    
+    public class ShoyuRamen
     {
-        public string Name;
-
         private Noodle _noodle;
         private Soup _soup;
         private List<Topping> _toppings = new List<Topping>();
 
-        public Ramen(Noodle noodle, Soup soup, List<Topping> toppings)
+        public ShoyuRamen(Noodle noodle, Soup soup, List<Topping> toppings)
         {
             _noodle = noodle;
             _soup = soup;
             _toppings = toppings;
         }
 
-        public int GetPrice()
+        public int GetCalory()
         {
-            int priceSum = 0;
-            priceSum += _noodle._price;
-            priceSum += _soup._price;
+            int kcalSum = 0;
+            kcalSum += _noodle._kcal;
+            kcalSum += _soup._kcal;
             foreach (var topping in _toppings)
             {
-                priceSum += topping._price;
+                kcalSum += topping._kcal;
             }
 
-            return priceSum;
+            return kcalSum;
         }
     }
 
     public class Noodle
     {
         public string _name;
-        public int _price;
+        public int _kcal;
 
-        public Noodle(string name, int price)
+        public Noodle(string name, int kcal)
         {
             _name = name;
-            _price = price;
+            _kcal = kcal;
         }
     }
 
     public class Soup
     {
         public string _name;
-        public int _price;
+        public int _kcal;
 
-        public Soup(string name, int price)
+        public Soup(string name, int kcal)
         {
             _name = name;
-            _price = price;
+            _kcal = kcal;
         }
     }
 
     public class Topping
     {
         public string _name;
-        public int _price;
+        public int _kcal;
 
-        public Topping(string name, int price)
+        public Topping(string name, int kcal)
         {
             _name = name;
-            _price = price;
+            _kcal = kcal;
         }
     }
 }
